e12d664e67d1ad81828fe4b21fcdb230b23aa871
LaveenK <laveenkewlani2004@gmail.com>
2025-09-13 16:24:08 +0530

    error correction

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.clientID,
       clientSecret: process.env.clientSecret,
-      callbackURL: "/auth/github/callback",
+      callbackURL: "https://chatzoom-ouv7.onrender.com/auth/github/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       //   console.log("Access Token:", accessToken);
